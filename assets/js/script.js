f98971a696e0ab6d4a5f48b81d6de0a5c3b66900
Ian L <ileach81@gmail.com>
2021-12-08 20:31:19 -0500

    job info loops

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Jooble api
 var url = "https://jooble.org/api/";
 var key = "114d40cd-a3ab-41a2-8549-cf04a44659fb";
 var params = "{ keywords: 'it', location: 'Columbus'}";
@@ -13,18 +14,23 @@ http.setRequestHeader("Content-type", "application/json");
 //Callback when the state changes
 http.onreadystatechange = function () {
   if (http.readyState == 4 && http.status == 200) {
-    console.log(JSON.parse(http.responseText));
+    for (let i = 0; i < JSON.parse(http.responseText).jobs.length; i++) {
+      var joobleOrgName = JSON.parse(http.responseText).jobs[i].company;
+      var joobleTitle = JSON.parse(http.responseText).jobs[i].title;
+      console.log(joobleOrgName);
+      console.log(joobleTitle);
+    }
   }
 };
 //Send request to the server
 http.send(params);
 
-//
-
+// usajobs api
 var host = "data.usajobs.gov";
 var userAgent = "ileach81@gmail.com";
 var authKey = "DOjDrxB7JE8vgKd3ajtL9XhA7+TQudbZSTT2N6tzPlo=";
-var url = "https://data.usajobs.gov/api/search?JobCategoryCode=2210";
+var url =
+  "https://data.usajobs.gov/api/search?Keyword=Developer&LocationName=Boulder,%20Colorado";
 fetch(url, {
   method: "GET",
   headers: {
@@ -37,5 +43,17 @@ fetch(url, {
     return response.json();
   })
   .then(function (data) {
-    console.log(data);
+    for (let i = 0; i < data.SearchResult.SearchResultItems.length; i++) {
+      var usaOrganizationName =
+        data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+          .OrganizationName;
+      var usaPosition =
+        data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+          .PositionTitle;
+
+      console.log(usaPosition);
+      console.log(usaOrganizationName);
+    }
   });
+
+// population of data
