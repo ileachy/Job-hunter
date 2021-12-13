a82eadd37edc66df4f99c31323c56d7e6b98ce87
AdamAlibraheem <shrineknight000@gmail.com>
2021-12-13 14:57:50 -0500

    USAjobsearch working

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,69 +10,77 @@ function searchFunction(event) {
   var newLi = document.createElement("li");
   newLi.textContent = searchTextJob;
   searchResults.appendChild(newLi);
-  intSearch(searchTextJob, searchTextCity);
+  var state = "Florida";
+  intSearchUSA(searchTextJob, searchTextCity, state);
 }
 
-// searchJob, searchCity
-function intSearch(searchJob, searchCity) {
-  // Jooble api
-  var url = "https://jooble.org/api/";
-  var key = "114d40cd-a3ab-41a2-8549-cf04a44659fb";
-  var params =
-    "{ keywords: '" + searchJob + "', location: '" + searchCity + "'}";
+// function intSearch(searchJob, searchCity) {
+//   // Jooble api
+//   var url = "https://jooble.org/api/";
+//   var key = "114d40cd-a3ab-41a2-8549-cf04a44659fb";
+//   var params =
+//     "{ keywords: '" + searchJob + "', location: '" + searchCity + "'}";
 
-  //create xmlHttpRequest object
-  var http = new XMLHttpRequest();
-  //open connection. true - asynchronous, false - synchronous
-  http.open("POST", url + key, true);
+//   //create xmlHttpRequest object
+//   var http = new XMLHttpRequest();
+//   //open connection. true - asynchronous, false - synchronous
+//   http.open("POST", url + key, true);
 
-  //Send the proper header information
-  http.setRequestHeader("Content-type", "application/json");
+//   //Send the proper header information
+//   http.setRequestHeader("Content-type", "application/json");
 
-  //Callback when the state changes
-  http.onreadystatechange = function () {
-    if (http.readyState == 4 && http.status == 200) {
-      for (let i = 0; i < JSON.parse(http.responseText).jobs.length; i++) {
-        var joobleOrgName = JSON.parse(http.responseText).jobs[i].company;
-        var joobleTitle = JSON.parse(http.responseText).jobs[i].title;
-        console.log(joobleOrgName);
-        console.log(joobleTitle);
-      }
-    }
-  };
-  //Send request to the server
-  http.send(params);
-}
+//   //Callback when the state changes
+//   http.onreadystatechange = function () {
+//     if (http.readyState == 4 && http.status == 200) {
+//       for (let i = 0; i < JSON.parse(http.responseText).jobs.length; i++) {
+//         var joobleOrgName = JSON.parse(http.responseText).jobs[i].company;
+//         var joobleTitle = JSON.parse(http.responseText).jobs[i].title;
+//         console.log(joobleOrgName);
+//         console.log(joobleTitle);
+//       }
+//     }
+//   };
+//   //Send request to the server
+//   http.send(params);
+// }
 
 // usajobs api
-var host = "data.usajobs.gov";
-var userAgent = "ileach81@gmail.com";
-var authKey = "DOjDrxB7JE8vgKd3ajtL9XhA7+TQudbZSTT2N6tzPlo=";
-var url =
-  "https://data.usajobs.gov/api/search?Keyword=Developer&LocationName=Boulder,%20Colorado";
-fetch(url, {
-  method: "GET",
-  headers: {
-    Host: host,
-    "User-Agent": userAgent,
-    "Authorization-Key": authKey,
-  },
-})
-  .then(function (response) {
-    return response.json();
+function intSearchUSA(searchJob, searchCity, searchState) {
+  var host = "data.usajobs.gov";
+  var userAgent = "ileach81@gmail.com";
+  var authKey = "DOjDrxB7JE8vgKd3ajtL9XhA7+TQudbZSTT2N6tzPlo=";
+  var url =
+    "https://data.usajobs.gov/api/search?Keyword=" +
+    searchJob +
+    "&LocationName=" +
+    searchCity +
+    ",%20" +
+    searchState;
+  //"https://data.usajobs.gov/api/search?Keyword=Developer&LocationName=Boulder,%20Colorado";
+  fetch(url, {
+    method: "GET",
+    headers: {
+      Host: host,
+      "User-Agent": userAgent,
+      "Authorization-Key": authKey,
+    },
   })
-  .then(function (data) {
-    for (let i = 0; i < data.SearchResult.SearchResultItems.length; i++) {
-      var usaOrganizationName =
-        data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
-          .OrganizationName;
-      var usaPosition =
-        data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
-          .PositionTitle;
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      for (let i = 0; i < data.SearchResult.SearchResultItems.length; i++) {
+        var usaOrganizationName =
+          data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+            .OrganizationName;
+        var usaPosition =
+          data.SearchResult.SearchResultItems[i].MatchedObjectDescriptor
+            .PositionTitle;
 
-      //   console.log(usaPosition);
-      //   console.log(usaOrganizationName);
-    }
-  });
+        console.log(usaPosition);
+        console.log(usaOrganizationName);
+      }
+    });
+}
 
 // population of data
